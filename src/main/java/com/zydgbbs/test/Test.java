41932925e4352dbaa4d95bb33cdedbeb44dc8696
zydgbbs <admin@zydgbbs.com>
2019-12-09 21:03:25 +0800

    sqlsessiondaoSupport

--- src/main/java/com/zydgbbs/test/Test.java
+++ src/main/java/com/zydgbbs/test/Test.java
@@ -8,7 +8,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserMapper userMapper = context.getBean("userMapper",UserMapper.class);
+
+        /*方式一UserMapper userMapper = context.getBean("userMapper",UserMapper.class);
+        User user = userMapper.getUserById(29);
+        System.out.println(user);*/
+
+        //方式三 mybatis plus，方式四 通用mapper
+        //方式二
+        UserMapper userMapper = context.getBean("userMapper2",UserMapper.class);
         User user = userMapper.getUserById(29);
         System.out.println(user);
     }
